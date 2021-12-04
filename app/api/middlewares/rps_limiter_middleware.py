683c1582215314be7e178b13e90592ac9ac1e14f
Сергей Лошкарев <saloshkarev@miem.hse.ru>
2021-12-04 13:59:17 +0300

    🔊 add log for rps_limiter

--- app/api/middlewares/rps_limiter_middleware.py
+++ app/api/middlewares/rps_limiter_middleware.py
@@ -1,7 +1,6 @@
 
 import logging
 from typing import Callable
-from http import HTTPStatus
 
 from aiohttp import web
 
@@ -10,5 +9,6 @@ logger = logging.getLogger(__name__)
 @web.middleware
 async def rps_limiter_middleware(request: web.Request, handler: Callable) -> web.Response:
     if await request.app['rps_limiter'].is_limit(request.rel_url):
+        logger.info(f'User get 429 status code on {request.rel_url}')
         raise web.HTTPTooManyRequests()
     return await handler(request)
