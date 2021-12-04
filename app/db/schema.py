6b55e61165cd6336cbacfc72174f3595f3aa3464
Olex1313 <aa1limonov@gmail.com>
2021-12-04 22:06:57 +0300

    :bug: Replace type for type_

--- app/db/schema.py
+++ app/db/schema.py
@@ -15,31 +15,31 @@ restaurants = Table(
     'Restaurants', meta,
     Column(name='id', type_=Integer, primary_key=True),
     Column(name='external_id', type_=UUID(as_uuid=False), default=uuid4),
-    Column(name='coords', type=String(60), nullable=False),
-    Column(name='name', type=String(60), nullable=False)
+    Column(name='coords', type_=String(60), nullable=False),
+    Column(name='name', type_=String(60), nullable=False)
 )
 
 orders = Table(
     'Orders', meta,
     Column(name='id', type_=Integer, primary_key=True),
     Column(name='external_id', type_=UUID(as_uuid=False), default=uuid4),
-    Column(name='content', type=String(60), nullable=False),
-    Column(name='comment', type=String(60), nullable=False),
+    Column(name='content', type_=String(60), nullable=False),
+    Column(name='comment', type_=String(60), nullable=False),
     Column(name='state', type_=states, nullable=False),
-    Column(name='restaurant_id', type=Integer, foreign_key=ForeignKey('Restaurants.id'))
+    Column(name='restaurant_id', type_=Integer, foreign_key=ForeignKey('Restaurants.id'))
 )
 
 dishes = Table(
     'Dishes', meta,
     Column(name='id', type_=Integer, primary_key=True),
     Column(name='external_id', type_=UUID(as_uuid=False), default=uuid4),
-    Column(name='price', type=Integer, default=0, nullable=False),
-    Column(name='restaurant_id', type=Integer, foreign_key=ForeignKey('Restaurants.id'))
+    Column(name='price', type_=Integer, default=0, nullable=False),
+    Column(name='restaurant_id', type_=Integer, foreign_key=ForeignKey('Restaurants.id'))
 )
 
 orders_to_dishes = Table(
     'OrdersToDishes', meta,
     Column(name='id', type_=Integer, primary_key=True),
-    Column(name='dish_id', type=Integer, foreign_key=ForeignKey('Dishes.id')),
-    Column(name='order_id', type=Integer, foreign_key=ForeignKey('Orders.id'))
+    Column(name='dish_id', type_=Integer, foreign_key=ForeignKey('Dishes.id')),
+    Column(name='order_id', type_=Integer, foreign_key=ForeignKey('Orders.id'))
 )
