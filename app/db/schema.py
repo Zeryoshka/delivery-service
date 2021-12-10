2d540ad2f95aaa6a5d9d3fbc23653315be5c977d
Сергей Лошкарев <saloshkarev@miem.hse.ru>
2021-12-11 01:06:49 +0300

    ✨ add naming convention for db

--- app/db/schema.py
+++ app/db/schema.py
@@ -4,12 +4,24 @@ from sqlalchemy.dialects.postgresql import UUID, ENUM
 from sqlalchemy import Table, Column, Integer, String, MetaData
 from sqlalchemy.sql.schema import ForeignKey
 
-meta = MetaData()
+
 class OrderState(Enum):
     RECEIVED = 1
     TAKEN = 2
     DELIVERED = 3
 
+convention = {
+    'all_column_names': lambda constraint, table: '_'.join([
+        column.name for column in constraint.columns.values()
+    ]),
+    'ix': 'ix__%(table_name)s__%(all_column_names)s',
+    'uq': 'uq__%(table_name)s__%(all_column_names)s',
+    'ck': 'ck__%(table_name)s__%(constraint_name)s',
+    'fk': 'fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s',
+    'pk': 'pk__%(table_name)s'
+}
+
+meta = MetaData(naming_convention=convention)
 
 restaurants = Table(
     'Restaurants', meta,
