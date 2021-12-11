ab76957e1e376ce7d4478b35260db2e77388ac46
Сергей Лошкарев <saloshkarev@miem.hse.ru>
2021-12-11 17:21:39 +0300

    ✨ add handlers according to OpenAPI orders

--- app/api/handlers/__init__.py
+++ app/api/handlers/__init__.py
@@ -1,6 +1,10 @@
 from app.api.handlers.ping import PingView, PingDBView
+from app.api.handlers.orders import OrdersView, OneOrderView, OrderStatusView
 
 handlers = [
     PingView,
-    PingDBView
+    PingDBView,
+    OrdersView,
+    OneOrderView,
+    OrderStatusView,
 ]
