4821a84a9ec24f18e566f70540c9cd700e3b20ad
Сергей Лошкарев <saloshkarev@miem.hse.ru>
2021-12-11 17:27:35 +0300

    ✨ add delivery/price handler

--- app/api/handlers/__init__.py
+++ app/api/handlers/__init__.py
@@ -1,3 +1,4 @@
+from app.api.handlers.delivery import DeliveryPriceView
 from app.api.handlers.ping import PingView, PingDBView
 from app.api.handlers.orders import OrdersView, OneOrderView, OrderStatusView
 
@@ -7,4 +8,5 @@ handlers = [
     OrdersView,
     OneOrderView,
     OrderStatusView,
+    DeliveryPriceView
 ]
