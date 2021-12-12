936e5fb4ca9f0723bc6467b8b40cad232b687751
Сергей Лошкарев <saloshkarev@miem.hse.ru>
2021-12-12 15:48:08 +0300

    ✨ add validation schema to delivery/price request

--- app/api/handlers/delivery.py
+++ app/api/handlers/delivery.py
@@ -1,8 +1,11 @@
 import logging
 from http import HTTPStatus
 from aiohttp import web
+from pydantic import ValidationError
 
 from app.api.handlers.base_view import BaseView
+from app.api.schemas.request_schemas import DeliveryPriceRequestSchema
+from app.geo_service.exceptions import GeoApiError, RouteNotFound
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +14,21 @@ class DeliveryPriceView(BaseView):
 
     async def post(self) -> web.Response:
         logger.info('Calc delivery price')
-        req = await self.request.json()
-        cost = await self.geo_service.get_cost(req['start'], req['end'])
+        try:
+            data = DeliveryPriceRequestSchema.parse_raw(
+                await self.request.text()
+            )
+        except ValidationError as err:
+            logger.debug(f'Error parsing delivery price: {err}')
+            raise web.HTTPBadRequest()
+
+        try:
+            cost = await self.geo_service.get_cost(data.start, data.end)
+        except RouteNotFound:
+            logger.info('Route not found by API')
+            raise web.HTTPNotFound()
+        except GeoApiError:
+            logger.info('GeoApi error')
+            raise web.HTTPInternalServerError()
+
         return web.json_response(data={'price': cost}, status=HTTPStatus.OK)
