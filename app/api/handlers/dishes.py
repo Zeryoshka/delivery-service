20877e094748b90b2833789dc0bbef889316c8f6
Сергей Лошкарев <saloshkarev@miem.hse.ru>
2021-12-13 03:30:48 +0300

    ♻️ refactor code for dishes handlers

--- app/api/handlers/dishes.py
+++ app/api/handlers/dishes.py
@@ -1,10 +1,11 @@
 from dataclasses import asdict
-import json
 import logging
 from http import HTTPStatus
 from aiohttp import web
+from pydantic import ValidationError
 
 from app.api.handlers.base_view import BaseView
+from app.api.schemas.request_schemas import CreateDishRequestSchema
 from app.db.dataclasses.dish import Dish
 from app.db.exceptions import DatabaseClientError
 
@@ -17,31 +18,37 @@ class OrderView(BaseView):
         logger.info('invoking dishes service')
         try:
             logger.info('invoking db method')
-            db_answer = await self.db.read_dishes()
-            logger.debug(f'db answer :{db_answer}')
-            return web.json_response(list(map(asdict, db_answer)))
+            db_response = await self.db.read_dishes()
+            logger.debug(f'db answer :{db_response}')
+            return web.json_response(
+                list(map(asdict, db_response))
+            )
         except DatabaseClientError as err:
-            return web.json_response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
+            logger.warn(f'DB error in get dishes handler: {err}')
+            raise web.HTTPInternalServerError()
 
     async def post(self) -> web.Response:
         logger.info('invoking dishes service')
         try:
-            incoming_body = await self.request.json()
-            logger.info('invoking db method')
-            db_answer = await self.db.create_dish(
-                Dish(
-                    price=incoming_body['price'],
-                    name=incoming_body['name'],
-                    restaurant_uuid=incoming_body['restaurant']
-                )
+            data = CreateDishRequestSchema.parse_raw(
+                await self.request.text()
             )
-            return web.json_response({ 'dish_id': str(uuid[0]) for uuid in db_answer }, status=HTTPStatus.CREATED)
-        except json.JSONDecodeError as err:
-            logger.error(err)
-            return web.json_response(status=HTTPStatus.BAD_REQUEST)
+            new_dish = Dish(
+                price=data.price,
+                name=data.name,
+                restaurant_uuid=data.restaurant
+            )
+        except ValidationError as err:
+            logger.debug(f'Error parsing delivery price: {err}')
+            raise web.HTTPBadRequest()
+
+        logger.info('invoking db method')
+        try:
+            db_answer = await self.db.create_dish(new_dish)
         except DatabaseClientError as err:
             logger.error(err)
-            return web.json_response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
+            raise web.HTTPInternalServerError()
+        return web.json_response({ 'dish_id': str(db_answer[0][0]) }, status=HTTPStatus.CREATED)
 
 class DishesView(BaseView):
     URL = r'/api/v1/dishes'
